ca8cb43c480fd3c16ba01be1a9144306fe593876
Usmanaftab4u <usmana9@gmail.com>
2020-02-20 00:16:44 +0500

    Transaction-User-UI

--- src/app/admins/user/user.component.ts
+++ src/app/admins/user/user.component.ts
@@ -8,11 +8,46 @@ import { MainscreenService } from './../../main-screen/mainscreen.service';
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
+  constructor(private service:AdminServiceService) { }
 
   allTransactions = [];
   userTransactions = [];
+  startValue: Date | null = null;
+  endValue: Date | null = null;
+  endOpen = false;
 
-  constructor(private service:AdminServiceService) { }
+  disabledStartDate = (startValue: Date): boolean => {
+    if (!startValue || !this.endValue) {
+      return false;
+    }
+    return startValue.getTime() > this.endValue.getTime();
+  };
+
+  disabledEndDate = (endValue: Date): boolean => {
+    if (!endValue || !this.startValue) {
+      return false;
+    }
+    return endValue.getTime() <= this.startValue.getTime();
+  };
+
+  onStartChange(date: Date): void {
+    this.startValue = date;
+  }
+
+  onEndChange(date: Date): void {
+    this.endValue = date;
+  }
+
+  handleStartOpenChange(open: boolean): void {
+    if (!open) {
+      this.endOpen = true;
+    }
+    console.log('handleStartOpenChange', open, this.endOpen);
+  }
+  handleEndOpenChange(open: boolean): void {
+    console.log(open);
+    this.endOpen = open;
+  }
 
   ngOnInit() {
     this.showTransactions();
