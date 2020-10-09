3bbac789146dc36cfc9666c57e31913573062bbd
nouman1998 <nouman.ejaz198@gmail.com>
2020-10-09 21:22:05 +0500

    setting Logo optional work

--- src/app/admins/setting/setting.component.ts
+++ src/app/admins/setting/setting.component.ts
@@ -15,21 +15,21 @@ export class SettingComponent implements OnInit {
 
   settingObj: setting = new setting();
   formData = new FormData();
-  Checker=false;
+  Checker = false;
   id
   extraTemplate
   ngOnInit() {
     this.adminService.getSetting().subscribe(d => {
 
-      this.Checker=true;
+      this.Checker = true;
       if (d && d.length != 0) {
-        this.id=d[0].id
+        this.id = d[0].id
         this.settingObj.header = d[0].header;
         this.settingObj.footer = d[0].footer;
-        this.imgURL=d[0].logo
+        this.imgURL = d[0].logo
         this.adminService.getImage(this.imgURL).subscribe(e => {
-          if(e){
-            this.settingObj.logo=e;
+          if (e) {
+            this.settingObj.logo = e;
           }
 
         })
@@ -37,29 +37,40 @@ export class SettingComponent implements OnInit {
 
 
     })
+    this.formData.delete('header');
+    this.formData.delete('logo');
+    this.formData.delete('footer');
   }
 
 
 
   submitSetting(myForm: NgForm) {
-    this.Checker=false;
+    this.Checker = false;
     this.formData.append('header', this.settingObj.header)
-    this.formData.append('logo', this.settingObj.logo,"settinglogo.png")
+    if (this.settingObj.logo) {
+      this.formData.append('logo', this.settingObj.logo, "settinglogo.png")
+    }
+    else{
+
+    }
+
     this.formData.append('footer', this.settingObj.footer)
 
 
 
 
     this.adminService.postSetting(this.formData).subscribe(d => {
+      this.formData.delete('header');
+      this.formData.delete('logo');
+      this.formData.delete('footer');
       this.message.success('Saved successfully', {
         nzDuration: 3000
+
       })
     });
 
     myForm.reset();
-    this.formData.delete('header');
-    this.formData.delete('logo');
-    this.formData.delete('footer');
+
     //this.formData.delete('headerName')
 
   }
